a0a857866b5ccd07b5efc5b1f2b8e0c63e634269
DESKTOP-8PCCSA3\Prism <yeolkeut@gmail.com>
2018-05-10 00:37:11 +0900

    upload my code

---
+++ test.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("todo.db")
+
+    cur = conn.cursor()
+
+    create_table = """create table if not exists todo(
+                id integer primary key autoincrement,
+                what text not null,
+                due text not null,
+                finished integer default 0);"""
+    cur.execute(create_table)
+    conn.close()
+
+def run_program():
+    while True:
+        print("Choose what to do:")
+        selection = input("(a: Add todo, l: List todo, m: Modify todo, q: Quit)? ")
+        print()
+        if selection == 'a':
+            add_todo()
+            print()
+        elif selection == 'l':
+            list_todo()
+            print()
+        elif selection == 'm':
+            modify_todo()
+            print()
+        elif selection == 'q':
+            break
+
+def add_todo():
+    what = input("Todo? ")
+    due = input("Due date? ")
+    conn = sqlite3.connect("todo.db")
+    cur = conn.cursor()
+    cur.execute("insert into todo (what, due) values (?, ?)", (what, due,))
+    conn.commit()
+    conn.close()
+
+def list_todo():
+    conn = sqlite3.connect("todo.db")
+    cur = conn.cursor()
+    cur.execute("select * from todo where 1")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+
+def modify_todo():
+    pass
+
+if __name__ == "__main__":
+    create_db()
+    run_program()
